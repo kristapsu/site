b428c3c6145af0b747a00f2130a5878b2131bc8e
kristapsu <steinbergs.kristaps@gmail.com>
2016-03-01 16:35:11 +0200

    Final update on 2016.03.01

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  //for php server - res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  // for php server - console.log('Example app listening on port 3000!');
+  console.log('working');
 });
