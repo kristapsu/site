5b2993e4b9e9876d4490416451a17e01bc73bf12
kristapsu <steinbergs.kristaps@gmail.com>
2016-03-01 10:20:07 +0200

    commit new changes with js with presaved global variables

---
+++ js/main.js
@@ -0,0 +1,28 @@
+require.config({
+  paths: {
+    'jquery': '/bower_components/jquery/dist/jquery',
+    'underscore': '/bower_components/underscore/underscore',
+    'backbone': '/bower_components/backbone/backbone',
+    'mustache': '/bower_components/mustache.js/mustache',
+    'text': '/bower_components/requirejs-text/text'
+  },
+  shim: {
+    'jquery': {
+      exports: '$'
+    },
+    'underscore': {
+      exports: '_'
+    },
+    'backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    'mustache': {
+      exports: 'Mustache'
+    }
+  }
+});
+
+require(['app'], function (App) {
+  App.initialize();
+});
